c4460f5baa94996c2531525dda91e7edf020a5a1
David Rehmat <me@dave.blue>
2020-10-18 20:01:16 -0400

    enforce HTTPS flag

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+const enforce = require('express-sslify');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+if (process.env.ENFORCE_TLS) {
+    app.use(enforce.HTTPS({ trustProtoHeader: true }))
+}
+
+app.use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
